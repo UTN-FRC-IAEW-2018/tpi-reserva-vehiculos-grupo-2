2b4217fa52e1e7deaf001f8b85f1b1f61ce2c948
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-06-02 00:09:42 -0300

    Agregado bootstrap a tp-web

--- tp-web/src/App.js
+++ tp-web/src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+          <a class="navbar-brand" href="#">TP IAEW 2018</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Vehículos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Reservas</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <main role="main" class="container">
+          <div class="starter-template">
+            <h1>Bootstrap starter template</h1>
+            <p class="lead">Use this document as a way to quickly start any new project.
+            <br /> 
+            All you get is this text and a mostly barebones HTML document.</p>
+          </div>
+        </main>
       </div>
     );
   }
