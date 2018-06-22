b11a233f6f19cbb03b5ad97813c729ad76b8f593
Guillermo Marel <guille.marcel04@gmail.com>
2018-06-22 17:57:54 -0300

    fasdgdhjk

--- tp-api/Controllers/PaisesController.cs
+++ tp-api/Controllers/PaisesController.cs
@@ -1,10 +1,18 @@
-﻿using SOAP_Serv;
+﻿
+using System.Net;
+using System.Net.Security;
+using System.Web;
+using System.Net.Http;
+using SOAP_Serv;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.IO;
+using RestSharp;
 
 namespace tp_api.Controllers
 {
@@ -17,6 +25,9 @@ namespace tp_api.Controllers
         public JsonResult Get()
         {
             var service = new WCFReservaVehiculosClient();
+
+            //service.CancelarReservaAsync();
+            
             var credentials = new Credentials();
             credentials.UserName = "grupo_nro2";
             credentials.Password = "crkVYDPh";
@@ -26,6 +37,43 @@ namespace tp_api.Controllers
 
             return json;
         }
+
+
+        [HttpGet("{code}")]
+        public JsonResult Get(string code)
+        {
+            /*
+            HttpClient client = new HttpClient();
+
+            var values = new Dictionary<string, string>
+            {
+                { "grant_type", "authorization_code" },
+                { "redirect_uri", "http://localhost:3000/callback" },
+                { "code",code }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+            content.Headers.Add("Authorization", "Basic dGVzdF9jbGllbnRfMTp0ZXN0X3NlY3JldA==");
+            content.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+            
+            
+            var response = client.PostAsync("http://ec2-54-87-197-49.compute-1.amazonaws.com/v1/oauth/tokens", content).Result;
+
+            var responseString = response.Content.ReadAsStringAsync().Result;
+
+            return responseString;
+            */
+            var client = new RestClient("http://ec2-54-87-197-49.compute-1.amazonaws.com/v1/oauth/tokens");
+            var request = new RestRequest(Method.POST);
+            //request.AddHeader("Postman-Token", "4f489d7e-596e-4335-b6c3-2beed8ccb791");
+            request.AddHeader("Authorization", "Basic dGVzdF9jbGllbnRfMTp0ZXN0X3NlY3JldA==");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("undefined", $"grant_type=authorization_code&code={code}&redirect_uri=https%3A%2F%2Fwww.example.com", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            return new JsonResult( response.Content);
+        }
+
+
 
     }
 }
