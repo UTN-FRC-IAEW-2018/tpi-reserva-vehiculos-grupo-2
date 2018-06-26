f031df4ef1d74ca8bd9bd535a561f4258f9f89a1
Guillermo Marel <guille.marcel04@gmail.com>
2018-06-26 10:15:34 -0300

    Parte Geografica lista

---
+++ tp-api/Clases/WService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SOAP_Serv;
+
+namespace Clases
+{
+    public class WService
+    {
+
+        private static WCFReservaVehiculosClient _service;
+        private static Credentials _cred;
+        public static WCFReservaVehiculosClient Service
+        {
+            get{
+                if (_service == null)
+                    _service = new WCFReservaVehiculosClient();
+                return _service;
+            }
+        }
+        public static Credentials Credential
+        {
+            get
+            {
+                if(_cred == null)
+                {
+                    _cred = new Credentials();
+                    _cred.UserName = "grupo_nro2";
+                    _cred.Password = "crkVYDPh";
+                }
+                return _cred;
+            }
+        }
+        private WService()
+        {
+
+        }
+
+    }
+}
---
+++ tp-api/Controllers/GeoController.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Net;
+using System.Net.Security;
+using System.Web;
+using System.Net.Http;
+using SOAP_Serv;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.IO;
+using RestSharp;
+using Clases;
+
+namespace tp_api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/geo")]
+    public class GeoController : Controller
+    {
+        // GET: api/Paises
+        [HttpGet, Route("pais")]
+        public JsonResult GetPaises()
+        {
+            var service = WService.Service;
+            ConsultarPaisesResponse1 result = service.ConsultarPaisesAsync(WService.Credential).Result;
+            
+            return Json(result.ConsultarPaisesResult.Paises);
+        }
+
+
+        [HttpGet("{pais:int}"), Route("ciudad")]
+        public JsonResult GetCiudades(int pais)
+        {
+            var service = WService.Service;
+
+            var req = new ConsultarCiudadesRequest();
+            req.IdPais = pais;
+
+            ConsultarCiudadesResponse1 result = service.ConsultarCiudadesAsync(WService.Credential, req).Result;
+
+            return Json(result.ConsultarCiudadesResult.Ciudades);
+        }
+
+
+
+    }
+}
